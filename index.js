f5784123ccd97968bdc291790cc176dfe11be06b
Mágila Morgana S. Ferreira <magila.ferreira@etec.sp.gov.br>
2020-10-30 23:25:43 -0300

    Configurando as rotas cadastrar + editar + deletar

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // importar o express, body-parser, handlebars e sequelize
 const express = require('express')
 const Handlebars = require('handlebars');
-const expressHandlebars = require('express-handlebars'), { alowInsecurePrototypeAccess, allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
+const expressHandlebars = require('express-handlebars'),
+    { alowInsecurePrototypeAccess, allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access')
 const bodyParser = require('body-parser')
 const Calendario = require('./models/Calendario')
 const Evento = require('./models/Evento')
@@ -12,7 +13,7 @@ const app = express();
 
 // Template handlebars
 app.engine('handlebars', expressHandlebars({
-  handlebars: allowInsecurePrototypeAccess(Handlebars),
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
 }));
 app.set('view engine', 'handlebars')
 app.use(express.static('public'))
@@ -25,103 +26,122 @@ app.use(bodyParser.json())
 // ************************************************* GERAL *************************************************
 // Rota - Tela HOME (front-end: escolhe calendário curso)
 app.get('/', (req, res) => {
-  res.render('home')
+    res.render('home')
 })
 
 // ************************************************* ADMIN *************************************************
 // Rota - Tela ADMIN (front-end: login)
 app.get('/adm', (req, res) => {
-  res.render('adm')
+    res.render('adm')
+})
+
+// Rota - Tela ADMIN (front-end: login)
+app.get('/edit/:id', (req, res) => {
+    const dados = ['FERIADO']
+    res.render('eventos', { dados })
 })
 
 // Rota - cadastro LOGIN (back-end: cadastra os dados das inputs no BD e redireciona: LOGIN ou HOME (front-end)) 
 app.post('/cadastro', (req, res) => {
-  Login.create({
-    RM: req.body.RM,
-    senha: req.body.senha
-  }).then(() => {
-    //      (front-end: redirecionando para LOGIN)
-    const { senha } = req.body
-    if (senha === '2020') {
-      res.redirect('cadastrados')
-    } else {
-      //          (front-end: redirecionando para HOME)
-      res.redirect('/')
-    }
-  }).catch((erro) => {
-    res.send("Houve um erro: " + erro)
-  })
+    Login.create({
+        RM: req.body.RM,
+        senha: req.body.senha
+    }).then(() => {
+        //      (front-end: redirecionando para LOGIN)
+        const { senha } = req.body
+        if (senha === '2020') {
+            res.redirect('cadastrados')
+        } else {
+            //          (front-end: redirecionando para HOME)
+            res.redirect('/')
+        }
+    }).catch((erro) => {
+        res.send("Houve um erro: " + erro)
+    })
 })
 
 // Rota - (front-end: retorna os dados do BD para o navegador)
 app.get('/logins', (req, res) => {
-  Login.findAll({ order: [['id', 'DESC']] }).then((logins) => {
-    res.render('logins', { logins: logins })
-  }).catch((erro) => {
-    res.send("Houve um erro: " + erro)
-  })
+    Login.findAll({
+        order: [
+            ['id', 'DESC']
+        ]
+    }).then((logins) => {
+        res.render('logins', { logins: logins })
+    }).catch((erro) => {
+        res.send("Houve um erro: " + erro)
+    })
 })
 
-// Rota - (back-end: deleta cadastros do BD)
+// Rota - deletar LOGINS (back-end: deleta cadastros do BD)
 app.get('/del/:id', (req, res) => {
-  Login.destroy({ where: { 'id': req.params.id } }).then(() => {
-    Login.findAll({ order: [['id', 'DESC']] }).then((logins) => {
-      res.render('logins', { logins: logins })
+    Login.destroy({ where: { 'id': req.params.id } }).then(() => {
+        Login.findAll({
+            order: [
+                ['id', 'DESC']
+            ]
+        }).then((logins) => {
+            res.render('logins', { logins: logins })
+        }).catch((erro) => {
+            res.send("Houve um erro: " + erro)
+        })
     }).catch((erro) => {
-      res.send("Houve um erro: " + erro)
+        res.send("Esta postagem não existe!")
     })
-  }).catch((erro) => {
-    res.send("Esta postagem não existe!")
-  })
+})
+
+// Rota - deletar CALENDÁRIOS (back-end: deleta cadastros do BD)
+app.get('/delet/:id', (req, res) => {
+    Calendario.destroy({ where: { 'id': req.params.id } }).then(() => {
+        Calendario.findAll({
+            order: [
+                ['id']
+            ]
+        }).then((calendarios) => {
+            res.render('cadastrados', { calendarios: calendarios })
+        }).catch((erro) => {
+            res.send("Houve um erro: " + erro)
+        })
+    }).catch((erro) => {
+        res.send("Esta postagem não existe!")
+    })
+})
+
+// Rota - (front-end: retorna os dados do BD para o navegador)
+app.get('/cadastrados', (req, res) => {
+    Calendario.findAll({
+        order: [
+            ['id', 'ASC']
+        ]
+    }).then((calendarios) => {
+        res.render('cadastrados', { calendarios: calendarios })
+    }).catch((erro) => {
+        res.send("Houve um erro: " + erro)
+    })
+})
+
+// Rota - chama o template cadastrar CALENDÁRIO (front-end: cadastrar calendários)
+app.get('/cadastrar', (req, res) => {
+    res.render('cadastrar')
+})
+
+// Rota - cadastra o CALENDARIO (back-end: cadastra os dados das inputs no BD e redireciona: CADASTRADOS (front-end)) 
+app.post('/cadastrando', (req, res) => {
+    Calendario.create({
+        ano: req.body.ano,
+        semestre: req.body.semestre
+    }).then(() => {
+        //  (front-end: redirecionando para CADASTRADOS)
+        res.redirect('cadastrados')
+    }).catch((erro) => {
+        res.send("Houve um erro: " + erro)
+    })
 })
 
 // ************************************************ USUÁRIO ************************************************
 // Rota - Tela CALENDÁRIO (front-end: visualiza calendário geral)
 app.get('/calendar', (req, res) => {
-  res.render('calendar')
-})
-
-// Rota - (front-end: retorna os dados do BD para o navegador)
-app.get('/cadastrados', (req, res) => {
-  Calendario.findAll({ order: [['id', 'ASC']] }).then((calendarios) => {
-    res.render('cadastrados', { calendarios: calendarios })
-  }).catch((erro) => {
-    res.send("Houve um erro: " + erro)
-  })
-})
-
-// Rota - EDITAR Eventos (front-end: altera dados dos calendarios cadastrados no BD)
-app.get('/edit/:ano:id', (req, res) => {
-  Calendario.update({ where: { 'id': req.params.id } }).then(() => {
-    Login.findAll({ order: [[ 'ano', 'id', 'ASC']] }).then((calendarios) => {
-      res.render('cadastrados', { calendarios: calendarios })
-    }).catch((erro) => {
-      res.send("Houve um erro: " + erro)
-    })
-  }).catch((erro) => {
-    res.send("Esta postagem não existe!")
-  })
-}) 
-
-
-// Rota - cadastrar CALENDÁRIO (front-end: cadastrar calendários)
-app.get('/cadastrar', (req, res) => {
-  const dados = ['FERIADO']
-  res.render('cadastrar', {dados})
-})
-
-// Rota - cadastro CALENDARIO (back-end: cadastra os dados das inputs no BD e redireciona: CADASTRADOS (front-end)) 
-app.post('/cadastrando', (req, res) => {
-  Calendario.create({
-    id: req.body.ano.ano,
-    ano: req.body.ano,
-    semestre: req.body.semestre
-  }).then(() => {
-    //  (front-end: redirecionando para CADASTRADOS)
-    res.redirect('cadastrados')
-  }).catch((erro) => {
-    res.send("Houve um erro: " + erro)
-  })
+    res.render('calendar')
 })
 
 // ******************************************************************
@@ -136,5 +156,5 @@ app.listen(8080)
 // **************************** Salvar no GitHub
 
 // git add . 
-// git commit -m "Criação do formulário de imagem" 
-// git push origin main 
+// git commit -m "Rotas cadastrando e cadastrados" 
+// git push origin main
--- models/Login.js
+++ models/Login.js
@@ -3,21 +3,20 @@ const bd = require('./bd')
 
 // ********* Criando MODELS referência da tabela dentro do sequelize - Vídeo 18 **********
 // MODEL 3 - Login
-const Login = bd.sequelize.define('logins',
-{
-  RM: { type: bd.Sequelize.TEXT },
-  senha: { type: bd.Sequelize.INTEGER },
+const Login = bd.sequelize.define('logins', {
+    RM: { type: bd.Sequelize.TEXT },
+    senha: { type: bd.Sequelize.INTEGER },
 })
 
 // ********** GERAR tabelas novas **********
-// Login.sync({force: true})
+// Login.sync({ force: true })
 
 /********** INSERT INTO TABLE logins **********
 //  INSERT 01
 Login.create(
 {
-    RM: "19051993",
-    senha: '2020'  
-})  */
+  RM: "19051993",
+  senha: '2020'  
+}) */
 
-module.exports = Login 
+module.exports = Login
